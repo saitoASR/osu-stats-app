45eda032573f91676735a939f1cc009d2dc49c41
saitoASR <satotakeru0909@gmail.com>
2025-06-26 04:00:51 +0900

    cssと微調整

--- resources/views/osu/index.blade.php
+++ resources/views/osu/index.blade.php
@@ -2,9 +2,10 @@
 <html>
 <head>
     <title>osu! ユーザー検索</title>
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
 </head>
 <body>
-    <h1>osu! ユーザー検索</h1>
+    <h1>osu!ユーザーを検索</h1>
     <form action="{{ route('osu.search') }}" method="POST">
         @csrf
         <input type="text" name="username" placeholder="ユーザー名を入力" required>
--- resources/views/osu/result.blade.php
+++ resources/views/osu/result.blade.php
@@ -1,16 +1,21 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>検索結果</title>
+    <link rel="stylesheet" href="{{ asset('css/result.css') }}">
 </head>
+
 <body>
     <h1>{{ $user['username'] }} の情報</h1>
+    <div class="padding">
     <img src="{{ $user['avatar_url'] }}" alt="プロフィール画像"><br>
-    <p>ランク: #{{ $user['statistics']['global_rank'] ?? 'N/A' }}</p>
-    <p>pp: {{ $user['statistics']['pp'] }}</p>
-    <p>レベル: {{ $user['statistics']['level']['current'] }}</p>
-    <p>国: {{ $user['country']['name'] }}</p>
-
+        <p>ランク: #{{ $user['statistics']['global_rank'] ?? 'N/A' }}</p>
+        <p>pp: {{ $user['statistics']['pp'] }}</p>
+        <p>レベル: {{ $user['statistics']['level']['current'] }}</p>
+        <p>国: {{ $user['country']['name'] }}</p>
+    </div>
     <a href="/">戻る</a>
 </body>
+
 </html>
